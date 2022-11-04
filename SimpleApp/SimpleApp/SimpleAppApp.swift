9f7114dbfe20a3205896a89d9d57a2c40816754a
Oswaldo Rubio <oswaldo.rubio@xing.com>
2022-11-04 15:06:11 +0100

    setup ios project

---
+++ SimpleApp/SimpleApp/SimpleAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SimpleAppApp.swift
+//  SimpleApp
+//
+//  Created by Oswaldo Rubio on 4/11/22.
+//
+
+import SwiftUI
+
+@main
+struct SimpleAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
