0a2c76f4b8555d7fa4f6cfb070093a8a9cb4de3f
Oswaldo Rubio <oswaldo.rubio@xing.com>
2023-02-14 09:25:59 +0100

    enable paralelization

--- SimpleApp/SimpleAppTests/SimpleAppTests2.swift
+++ SimpleApp/SimpleAppTests/SimpleAppTests2.swift
@@ -1,13 +1,13 @@
 //
-//  SimpleAppTests.swift
+//  SimpleAppTests2.swift
 //  SimpleAppTests
 //
-//  Created by Oswaldo Rubio on 9/11/22.
+//  Created by Oswaldo Rubio on 13/2/23.
 //
 
 import XCTest
 
-final class SimpleAppTests: XCTestCase {
+final class SimpleAppTests2: XCTestCase {
 
     override func setUpWithError() throws {
         // Put setup code here. This method is called before the invocation of each test method in the class.
@@ -16,6 +16,7 @@ final class SimpleAppTests: XCTestCase {
     override func tearDownWithError() throws {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
     }
+
     func testExample1() throws { sleep(4) }
     func testExample2() throws { sleep(4) }
     func testExample3() throws { sleep(4) }
@@ -26,7 +27,6 @@ final class SimpleAppTests: XCTestCase {
     func testExample8() throws { sleep(4) }
     func testExample9() throws { sleep(4) }
     func testExample10() throws { sleep(4) }
-  
+
 
 }
-
