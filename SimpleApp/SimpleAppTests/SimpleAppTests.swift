537099fd6374929b8861963a53af31b8cbe5d2d2
Oswaldo Rubio <oswaldo.rubio@xing.com>
2023-02-14 11:33:22 +0100

    increase time

--- SimpleApp/SimpleAppTests/SimpleAppTests.swift
+++ SimpleApp/SimpleAppTests/SimpleAppTests.swift
@@ -16,16 +16,16 @@ final class SimpleAppTests: XCTestCase {
     override func tearDownWithError() throws {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
     }
-    func testExample1() throws { sleep(10) }
-    func testExample2() throws { sleep(10) }
-    func testExample3() throws { sleep(10) }
-    func testExample4() throws { sleep(10) }
-    func testExample5() throws { sleep(10) }
-    func testExample6() throws { sleep(10) }
-    func testExample7() throws { sleep(10) }
-    func testExample8() throws { sleep(10) }
-    func testExample9() throws { sleep(10) }
-    func testExample10() throws { sleep(10) }
+    func testExample1() throws { sleep(30) }
+    func testExample2() throws { sleep(30) }
+    func testExample3() throws { sleep(30) }
+    func testExample4() throws { sleep(30) }
+    func testExample5() throws { sleep(30) }
+    func testExample6() throws { sleep(30) }
+    func testExample7() throws { sleep(30) }
+    func testExample8() throws { sleep(30) }
+    func testExample9() throws { sleep(30) }
+    func testExample10() throws { sleep(30) }
   
 
 }
